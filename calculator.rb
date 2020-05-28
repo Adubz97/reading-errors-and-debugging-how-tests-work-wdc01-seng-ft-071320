baea960b50bd37e21a88c90ae4c215f399ddcbfa
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-05-28 23:25:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -7,5 +7,11 @@ second_number != 0
 
 sum = first_number + second_number 
 
+difference = first_number + second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
+
 
 
