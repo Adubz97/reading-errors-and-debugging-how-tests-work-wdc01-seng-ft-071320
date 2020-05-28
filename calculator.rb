fcc7f13897e2a753fa79695dfb70d8bdbb2e9950
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-05-28 23:26:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -8,7 +8,7 @@ second_number != 0
 
 sum = first_number + second_number 
 
-difference = first_number + second_number
+difference = first_number - second_number
 
 product = first_number * second_number
 
